d5c93b11f81309c9a7348b41c7f81676d816c86b
SathyaSathvik <sathyasathvik7871@gmail.com>
2026-02-07 16:11:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from datetime import datetime
+from database import SessionLocal, Attendance
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Attendance(BaseModel):
+    student_id: str
+    name: str
+
+@app.get("/")
+def home():
+    return {"message": "Hello Backend"}
+
+@app.post("/mark-attendance")
+def mark_attendance(data: Attendance):
+    db = SessionLocal()
+    record = Attendance(student_id=name)
+    db.add(record)
+    db.commit()
+    db.close()
+
+    return {
+    "status": "attendance_marked",
+    "student_id": name,
+    "time": record.time
+    }
+@app.get("/test_db")
+def test_db():
+    db = SessionLocal()
+    return {"status": "database connected"}
+@app.get("/all_data")
+def all_data():
+    db = SessionLocal()
+    data = db.query(Attendance).all()
+    return [{"name": a.name, "time": a.time} for a in data]
+
